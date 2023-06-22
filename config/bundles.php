8532dffa7e08c251b2081ce6986ee4c7cf6a3aa4
someniatko <someniatko@gmail.com>
2023-06-22 16:34:47 +0300

    exclude irrelevant bundles

--- config/bundles.php
+++ config/bundles.php
@@ -2,8 +2,4 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Ecotone\SymfonyBundle\EcotoneSymfonyBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
